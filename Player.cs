5cc8731e6fadd02d93c7dbc87da3ace4fb914bbe
Daham Belpage <73273979+DahamBelpage@users.noreply.github.com>
2020-11-05 08:25:57 +0530

    Fix typo

--- Player.cs
+++ Player.cs
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour{
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);11
+        animator.SetFloat("Speed", movement.sqrMagnitude);
 
     }
 
