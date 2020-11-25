f2af47720496dd39deb8be62d154cfc598db3510
DahamBelpage <dahamsbelpage@gmail.com>
2020-11-25 20:48:37 +0530

    Add basic controls

--- PlayerController.cs
+++ PlayerController.cs
@@ -9,12 +9,55 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb;
 
+    private bool facingRight = true;
+    
+    //Grounded?
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
     private void Start() {
+        extraJumps = extraJumpsValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update() {
+        if(isGrounded == true){
+            extraJumps = extraJumpsValue;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && extraJumps > 0){
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
     private void FixedUpdate() {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
         moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0){
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0){
+            Flip();
+        }
+    }
+
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 }
