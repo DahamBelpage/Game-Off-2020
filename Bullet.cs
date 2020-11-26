f81f8a8b718f79b9a6e37a82b49777fcd656a071
DahamBelpage <dahamsbelpage@gmail.com>
2020-11-26 16:32:58 +0530

    Shooting and enemy death addition

---
+++ Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    public int damage = 20;
+    
+    private void Start() {
+        rb.velocity = transform.right * speed;
+        Destroy(gameObject, 10f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo) {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null){
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Shooting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private void Update() {
+        if(Input.GetButtonDown("Fire2")){
+            Shoot();
+        }
+    }
+
+    void Shoot(){
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
