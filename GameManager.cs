8d8e1e566e9281a2f8fba9ca42cbc7eebae41294
DahamBelpage <dahamsbelpage@gmail.com>
2020-11-28 11:45:02 +0530

    Basic main menu and cuple teawks

---
+++ GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+    public Animator mainMenuAnim, gameMenuAnim;
+
+    public void MainMenuSlideOut(){
+        mainMenuAnim.SetTrigger("SlideOut");
+        gameMenuAnim.SetTrigger("FadeIn");
+    }    
+}
